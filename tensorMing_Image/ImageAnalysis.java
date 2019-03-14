0e6743213ce48a4b1358fcf58d85ad3fbbbc3e79
Ming <chenmingxiang110@gmail.com>
2019-03-14 12:15:11 +0800

    Add files via upload

--- tensorMing_Image/ImageAnalysis.java
+++ tensorMing_Image/ImageAnalysis.java
@@ -2,12 +2,74 @@ package tensorMing_Image;
 
 public class ImageAnalysis {
 
-    public static boolean[][] simpleEdge(int[][][] image) {
-        return null;
+    private static int intArraySum(int[] input) {
+        int result = 0;
+        for (int i : input) {
+            result+=i;
+        }
+        return result;
     }
 
-    public static boolean[][] cannyEdge(int[][][] image) {
-        return null;
+    public static boolean[][] simpleEdge(int[][][] image, int threshold) {
+        boolean[][] result = new boolean[image.length][image[0].length];
+        for (int x = 0 ; x<image.length ; x++) {
+            for (int y = 0 ; y<image[0].length ; y++) {
+                double sum = 0;
+                double num = 0;
+                if (x>0) {
+                    sum+=intArraySum(image[x][y])-intArraySum(image[x-1][y]);
+                    num++;
+                }
+                if (x<(image.length-1)) {
+                    sum+=intArraySum(image[x][y])-intArraySum(image[x+1][y]);
+                    num++;
+                }
+                if (y>0) {
+                    sum+=intArraySum(image[x][y])-intArraySum(image[x][y-1]);
+                    num++;
+                }
+                if (y<(image[0].length-1)) {
+                    sum+=intArraySum(image[x][y])-intArraySum(image[x][y+1]);
+                    num++;
+                }
+                if ((sum/num)>=threshold) {
+                    result[x][y] = true;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static boolean[][] soberEdge(int[][][] image, double threshold) {
+        double[][][] soberResult = ImageFilter.sober(image);
+        double[][] gradDist = soberResult[2];
+        boolean[][] result = new boolean[gradDist.length][gradDist[0].length];
+
+        for (int x = 0 ; x<gradDist.length ; x++) {
+            for (int y = 0 ; y<gradDist[0].length ; y++) {
+                if (gradDist[x][y]>=threshold) {
+                    result[x][y] = true;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static boolean[][] cannyEdge(int[][][] image, double lowerBound, double upperBound) {
+        int[][][] gaussian = ImageFilter.gaussianBlur(image, 5, 1.4f);
+        double[][][] soberResult = ImageFilter.sober(gaussian);
+        double[][] suppressionResult = ImageFilter.nonMaximumSuppression(soberResult);
+
+        boolean[][] result = new boolean[suppressionResult.length][suppressionResult[0].length];
+
+        for (int x = 0 ; x<suppressionResult.length ; x++) {
+            for (int y = 0 ; y<suppressionResult[0].length ; y++) {
+                if (suppressionResult[x][y]>=lowerBound && suppressionResult[x][y]<=upperBound) {
+                    result[x][y] = true;
+                }
+            }
+        }
+        return result;
     }
 
 }
--- tensorMing_Image/ImageTests.java
+++ tensorMing_Image/ImageTests.java
@@ -45,6 +45,59 @@ public class ImageTests {
 //        ImageFileIO.writeImage("mean25.jpg", data);
 //        data = ImageFilter.medianBlur(result, 25);
 //        ImageFileIO.writeImage("median25.jpg", data);
+
+//        int[][][] result = ImageFileIO.readImage("test_image.jpg");
+//        long startingTime = System.currentTimeMillis();
+//        int[][][] data = ImageFilter.gaussianBlur_legacy(result, 15, 10);
+//        System.out.println(System.currentTimeMillis()-startingTime);
+//        ImageFileIO.writeImage("gaussianBlur_legacy.jpg", data);
+//
+//        startingTime = System.currentTimeMillis();
+//        data = ImageFilter.gaussianBlur(result, 15, 10);
+//        System.out.println(System.currentTimeMillis()-startingTime);
+//        ImageFileIO.writeImage("gaussianBlur.jpg", data);
+
+//        int[][][] result = ImageFileIO.readImage("test_image.jpg");
+//
+//        long startingTime = System.currentTimeMillis();
+//        boolean[][] edge = ImageAnalysis.simpleEdge(result, 20);
+//        int[][][] edgePic = ImageDraw.bool2Pic(edge);
+//        System.out.println(System.currentTimeMillis()-startingTime);
+//        ImageFileIO.writeImage("edge20.jpg", edgePic);
+//
+//        startingTime = System.currentTimeMillis();
+//        edge = ImageAnalysis.simpleEdge(result, 50);
+//        edgePic = ImageDraw.bool2Pic(edge);
+//        System.out.println(System.currentTimeMillis()-startingTime);
+//        ImageFileIO.writeImage("edge50.jpg", edgePic);
+//
+//        startingTime = System.currentTimeMillis();
+//        edge = ImageAnalysis.simpleEdge(result, 100);
+//        edgePic = ImageDraw.bool2Pic(edge);
+//        System.out.println(System.currentTimeMillis()-startingTime);
+//        ImageFileIO.writeImage("edge100.jpg", edgePic);
+
+//        int[][][] result = ImageFileIO.readImage("test_image_2.jpg");
+//
+//        long startingTime = System.currentTimeMillis();
+//        boolean[][] edge = ImageAnalysis.soberEdge(result, 100);
+//        int[][][] edgePic = ImageDraw.bool2Pic(edge);
+//        System.out.println(System.currentTimeMillis()-startingTime);
+//        ImageFileIO.writeImage("soberEdge100.jpg", edgePic);
+//
+//        startingTime = System.currentTimeMillis();
+//        edge = ImageAnalysis.soberEdge(result, 200);
+//        edgePic = ImageDraw.bool2Pic(edge);
+//        System.out.println(System.currentTimeMillis()-startingTime);
+//        ImageFileIO.writeImage("soberEdge200.jpg", edgePic);
+
+        int[][][] result = ImageFileIO.readImage("test_image_2.jpg");
+
+        long startingTime = System.currentTimeMillis();
+        boolean[][] edge = ImageAnalysis.cannyEdge(result, 100, 1600);
+        int[][][] edgePic = ImageDraw.bool2Pic(edge);
+        System.out.println(System.currentTimeMillis()-startingTime);
+        ImageFileIO.writeImage("cannyEdge100.jpg", edgePic);
     }
 
 }
